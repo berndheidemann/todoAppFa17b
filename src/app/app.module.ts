a2e6c86f00934552aa46e0c475163b4f68bbf4d5
Bernd Heidemann <bernd.heidemann@googlemail.com>
2019-09-26 12:38:07 +0200

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { ListComponent } from './components/list/list.component';
-import { FormComponent } from './components/form/form.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppComponent} from './app.component';
+import {ListComponent} from './components/list/list.component';
+import {FormComponent} from './components/form/form.component';
 import {FormsModule} from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  {path: 'list', component: ListComponent},
+  {path: 'new', component: FormComponent},
+  {
+    path: '',
+    redirectTo: '/list',
+    pathMatch: 'full'
+  }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    FormComponent
+    FormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
